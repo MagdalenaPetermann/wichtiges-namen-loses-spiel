0752a705b46596ff0568610ca6bebde4f8d6a92e
MagdalenaPetermann <131597665+MagdalenaPetermann@users.noreply.github.com>
2023-04-23 15:24:17 +0200

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,116 @@
+let Buchstabe = 0
+let level = 0
+input.onButtonPressed(Button.A, function () {
+    Buchstabe = randint(1, 26)
+    if (Buchstabe == 1) {
+        basic.showString("a")
+    }
+    if (Buchstabe == 2) {
+        basic.showString("b")
+    }
+    if (Buchstabe == 3) {
+        basic.showString("c")
+    }
+    if (Buchstabe == 4) {
+        basic.showString("d")
+    }
+    if (Buchstabe == 5) {
+        basic.showString("e")
+    }
+    if (Buchstabe == 6) {
+        basic.showString("f")
+    }
+    if (Buchstabe == 7) {
+        basic.showString("g")
+    }
+    if (Buchstabe == 8) {
+        basic.showString("h")
+    }
+    if (Buchstabe == 9) {
+        basic.showString("i")
+    }
+    if (Buchstabe == 10) {
+        basic.showString("j")
+    }
+    if (Buchstabe == 11) {
+        basic.showString("k")
+    }
+    if (Buchstabe == 12) {
+        basic.showString("l")
+    }
+    if (Buchstabe == 13) {
+        basic.showString("m")
+    }
+    if (Buchstabe == 14) {
+        basic.showString("n")
+    }
+    if (Buchstabe == 15) {
+        basic.showString("o")
+    }
+    if (Buchstabe == 16) {
+        basic.showString("p")
+    }
+    if (Buchstabe == 17) {
+        basic.showString("Q")
+    }
+    if (Buchstabe == 18) {
+        basic.showString("r")
+    }
+    if (Buchstabe == 19) {
+        basic.showString("s")
+    }
+    if (Buchstabe == 20) {
+        basic.showString("t")
+    }
+    if (Buchstabe == 21) {
+        basic.showString("u")
+    }
+    if (Buchstabe == 22) {
+        basic.showString("v")
+    }
+    if (Buchstabe == 23) {
+        basic.showString("w")
+    }
+    if (Buchstabe == 24) {
+        basic.showString("x")
+    }
+    if (Buchstabe == 25) {
+        basic.showString("y")
+    }
+    if (Buchstabe == 26) {
+        basic.showString("z")
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    level = randint(0, 10)
+    if (level == 1) {
+        basic.showString("Essen")
+    }
+    if (level == 2) {
+        basic.showString("Buch")
+    }
+    if (level == 3) {
+        basic.showString("Tier")
+    }
+    if (level == 4) {
+        basic.showString("Promi")
+    }
+    if (level == 5) {
+        basic.showString("Wohnzimmer")
+    }
+    if (level == 6) {
+        basic.showString("Mädchen name")
+    }
+    if (level == 7) {
+        basic.showString("Jungs name")
+    }
+    if (level == 8) {
+        basic.showString("Sportart")
+    }
+    if (level == 9) {
+        basic.showString("hobby")
+    }
+    if (level == 10) {
+        basic.showString("Job")
+    }
+})
